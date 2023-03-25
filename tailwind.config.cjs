e8f1073516f67823d5ba0fd722448ffa9ff6cdb9
coder-showkat <showkat.coder@gmail.com>
2023-03-26 02:50:44 +0600

    added cssnano to postcss config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
+  purge:{
+    enabled: process.env.NODE_ENV !== "production",
+    safelist: [],
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    ]
+  },
   theme: {
     extend: {
       fontFamily: {
